41e3d60085e24f4ca8e8c8666ff89c52b3c122f4
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-04-24 08:17:13 -0700

    login and register work using session based auth

--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-
+const session = require('express-session');
+const sessionConfig = require('./sessionConfig');
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
@@ -11,8 +12,12 @@ const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(session(sessionConfig))
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
 
+server.use('/', (req, res) => {
+  res.status(200).json({ message: 'Server is Live' })
+})
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,42 @@
 const router = require('express').Router();
+const Users = require('./usersModel');
+const bcrypt = require('bcrypt');
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post('/register', newUserVerification, (req, res) => {
+  const user = req.body;
+  const rounds = process.env.HASH_ROUNDS || 12
+  const hash = bcrypt.hashSync(user.password, rounds)
+  user.password = hash
+  
+  Users.add(user)
+    .then(addedUser => {
+      console.log(addedUser)
+      res.status(201).json({ message: 'user created successfully.', addedUser })
+    })
+    .catch(err => res.status(500).json({ errorMessage: `Internal server error`, err }))
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  const { username, password } = req.body;
+
+  Users.findBy({ username })
+    .then(([user]) => {
+      if(user && bcrypt.compareSync(password, user.password)) {
+        req.session.loggedIn = true
+        res.status(200).json({ message: `Permitted Access.` });
+      } else {
+        res.status(401).json({ message: `Unauthorized Access, please login.` })
+      }
+    })
+    .catch(err => res.status(500).json({ errorMessage: `Internal server error`, err }))
 });
 
 module.exports = router;
+
+function newUserVerification(req, res, next) {
+  const user = req.body;
+
+  if((user.username && user.password) && (user.username.length > 3 && user.password.length > 5)) {
+    next()
+  }
+}
